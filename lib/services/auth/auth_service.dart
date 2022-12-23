15203a0f42ba27f5d4aebe60f45ba6e39a812591
Juan Barreto <juan2.manuelbarreto@gmail.com>
2022-12-22 21:19:16 -0400

    step-8

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,43 @@
+import 'auth_provider.dart';
+import 'auth_user.dart';
+
+class AuthService implements AuthProvider {
+  final AuthProvider provider;
+  const AuthService(this.provider);
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) =>
+      provider.createUser(
+        email: email,
+        password: password,
+      );
+
+  @override
+  // TODO: implement currentUser
+  AuthUser? get currentUser => provider.currentUser;
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) =>
+      provider.logIn(
+        email: email,
+        password: password,
+      );
+
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+}
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
-import 'package:mynotes/views/register_view.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
